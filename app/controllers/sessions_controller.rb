6910fc167072c6af3b740a495d814dc23de24952
Michael Dilley <mike.dilley@gmail.com>
2019-02-01 16:13:20 -0600

    Add SessionsController#create and sessions new view to login users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
-
+    if find_user
+      @user.authenticate(params[:password])
+      login(@user)
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
 
+  private
+
+    def find_user
+      @user = User.find_by(email: params[:email])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :dogs
   root 'welcome#index'
   resources :users, only: [:new, :create, :show, :update, :edit]
-  get '/signup' => 'sessions#new'
-  get '/login' => 'sessions#create'
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

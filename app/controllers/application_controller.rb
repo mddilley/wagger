c00c1fde0a20dda39fdfd633c80cd74c11bf9767
Michael Dilley <mike.dilley@gmail.com>
2019-02-01 14:23:02 -0600

    Add #current_user and #logged_in? to ApplicationsController, style welcome index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
 
   def login(user)
     session[:id] = user.id
+  end
+
+  def logged_in?
+    session[:id]
+  end
+
+  def current_user
+    User.find(session[:id]) if logged_in?
   end
 end

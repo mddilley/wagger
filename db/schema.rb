7bd21f807f6f7997d2c7e5b6c474b126095cc1fb
Michael Dilley <mike.dilley@gmail.com>
2019-02-03 14:06:41 -0600

    Add associations to User and Dog, add user_id attr to Dog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_212215) do
+ActiveRecord::Schema.define(version: 2019_02_03_200351) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_212215) do
     t.integer "aggressive_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

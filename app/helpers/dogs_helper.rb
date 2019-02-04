29e068bcfbe902b93473b991899d9004bca6feb7
Michael Dilley <mike.dilley@gmail.com>
2019-02-04 16:09:50 -0600

    Modify dogs#new - add age and breed fields, add dogs_helper#breeds_for_select

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,2 +1,8 @@
 module DogsHelper
+
+  def breeds_for_select
+    breed = open('https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/dogs.json')
+    JSON.parse(breed.read)["dogs"]
+  end
+
 end

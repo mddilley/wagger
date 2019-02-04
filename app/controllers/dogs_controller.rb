2e701cbb42703d2ae26dea754b374c8f42e1362c
Michael Dilley <mike.dilley@gmail.com>
2019-02-04 16:22:56 -0600

    Add all dog attributes to form in dogs new view, add dog attributes to dog _params in DogsController

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -23,6 +23,6 @@ class DogsController < ApplicationController
   private
 
     def dog_params
-      params.require(:dog).permit(:name, :sex)
+      params.require(:dog).permit(:name, :sex, :breed, :age, :weight, :friendly_rating, :aggressive_rating, :fixed)
     end
 end

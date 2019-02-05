b0437a898703cc75e4c7176e50ccf85f0f45cb7c
Michael Dilley <mike.dilley@gmail.com>
2019-02-05 16:49:50 -0600

    Change color of logout button (modify Bootstrap class)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,33 +2,14 @@ module ApplicationHelper
 
   def show_navbar
     <<-HTML
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <strong><span class="navbar-brand" >Wagger</span></strong>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#{user_dogs_path(current_user)}">My Dogs<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">My Playdates</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#{user_path(current_user)}">Preferences</a>
-            </li>
-          </ul>
-        </div>
-        #{logout_button}
-      </nav>
+
     HTML
   end
 
   def logout_button
     if logged_in?
       <<-HTML
-            <div><a class="btn btn-danger btn-sm" href="#{logout_path}" role="button">Logout</a></div>
+            <div><a class="btn btn-warning btn-sm" href="#{logout_path}" role="button">Logout</a></div>
       HTML
     end
   end

0b21642e1c4f88f212ff2d5a16e67648e9f03249
Michael Dilley <mike.dilley@gmail.com>
2019-02-11 14:38:09 -0600

    Add PlayDate#format_date and PlayDate#format_time

--- app/models/play_date.rb
+++ app/models/play_date.rb
@@ -1,4 +1,13 @@
 class PlayDate < ApplicationRecord
   has_many :dog_play_dates
   has_many :dogs, through: :dog_play_dates
+
+  def format_date
+    date.strftime("%B %d, %Y")
+  end
+
+  def format_time
+    time.strftime("%I:%M %p")
+  end
+
 end

ee5276b0a0599f3e3f6cf7e7b15b2b55c12c5243
Michael Dilley <mike.dilley@gmail.com>
2019-02-13 16:49:44 -0600

    Add Bootstrap classes to forms in /sessions/new and /users/new views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_many :play_dates, through: :dogs
 
   accepts_nested_attributes_for :dogs
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
 end

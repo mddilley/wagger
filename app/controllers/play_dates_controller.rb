24a10e1384e3887739abbfcf7c517f81625815d5
Michael Dilley <mike.dilley@gmail.com>
2019-02-20 10:01:38 -0600

    Add PlayDatesController#current_user_dogs and refactor PlayDatesController#(new, create, and edit) with it, style name and breed fields with Bootstrap in /dogs/_new_dog_form

--- app/controllers/play_dates_controller.rb
+++ app/controllers/play_dates_controller.rb
@@ -6,12 +6,12 @@ class PlayDatesController < ApplicationController
   def new
     @playdate = PlayDate.new
     @playdate.dog_play_dates.build
-    @dogs = current_user.dogs
+    current_user_dogs
   end
 
   def create
     @playdate = PlayDate.new(play_date_params)
-    @dogs = current_user.dogs
+    current_user_dogs
     if @playdate.save
       redirect_to play_date_path(@playdate)
     else
@@ -20,17 +20,17 @@ class PlayDatesController < ApplicationController
   end
 
   def show
-    @playdate = PlayDate.find(params[:id])
+    find_play_date
   end
 
   def edit
-    @playdate = PlayDate.find(params[:id])
+    find_play_date
     @playdate.dog_play_dates.build
-    @dogs = current_user.dogs
+    current_user_dogs
   end
 
   def update
-    @playdate = PlayDate.find(params[:id])
+    find_play_date
     @playdate.update(play_date_params)
     redirect_to play_date_path(@playdate)
   end
@@ -60,10 +60,14 @@ class PlayDatesController < ApplicationController
     end
 
     def find_play_date
-      PlayDate.find(params[:id])
+      @playdate = PlayDate.find(params[:id])
     end
 
     def play_date_owner?
       find_play_date.id == current_user.id
+    end
+
+    def current_user_dogs
+      @dogs = current_user.dogs
     end
 end

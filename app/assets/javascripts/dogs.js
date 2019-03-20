f27cb7249e386309ce8fcec30f05315ef66d49a8
Michael Dilley <mike.dilley@gmail.com>
2019-03-20 16:31:43 -0500

    Add handlebarsjs cdn to /layout/application view, add handlebars template to /dog/index view, render template data with AJAX response in dogs.js

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,11 +1,15 @@
-function hello(){
+function listDogs(){
   let userId = $("a.btn.btn-success.btn-lg").data("id");
   $.get("/users/" + userId + "/dogs", function(json){
-    json.map(obj => console.log(obj.name))
+    // json.map(obj => console.log(obj.name))
+    const source = $("#dogs-template").html();
+    const template = Handlebars.compile(source);
+    const content = template(json);
+    $("div.container.text-left.text-light.align-middle.form-group").append(content);
   });
 }
 
 $(function(){
-  hello();
+  listDogs();
 
 });

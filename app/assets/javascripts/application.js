332c59a7797e513286d260c0094b287b068e82b2
Michael Dilley <mike.dilley@gmail.com>
2019-03-20 13:16:39 -0500

    Remove turbolinks gem, remove require tree sprocket from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,4 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require jquery
-//= require_tree .
--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,11 +1,11 @@
 function hello(){
   let userId = $("a.btn.btn-success.btn-lg").data("id");
   $.get("/users/" + userId + "/dogs", function(json){
-    console.log(json);
+    json.map(obj => console.log(obj.name))
   });
 }
 
 $(function(){
   hello();
-  
+
 });

fa7790f539892fc54be8545947c86b703c217f59
Michael Dilley <mike.dilley@gmail.com>
2019-03-21 17:17:37 -0500

    Add fillDogArray(), checkPrev(), checkNext(), prevDog(), and nextDog() to async load next and prev dogs in /dog/show view

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -1,3 +1,5 @@
+let dogArray = [];
+
 function registerHelpers(){
 Handlebars.registerHelper('bootstrapRows', function(perRow, context, options) {
   let out = "", subcontext = [], i;
@@ -52,8 +54,66 @@ function showDog(){
   });
 }
 
+function nextDog(){
+  let userId = $("div.dog-show").data("uid");
+  let id = $("div.dog-show").data("id");
+  id = checkNext(userId, id);
+  $.get("/users/" + userId + "/dogs/" + id, function(json){
+    const source = $("#dog-template").html();
+    const template = Handlebars.compile(source);
+    const content = template(json);
+    $("div.dog-show").hide().html(content).fadeIn();
+  });
+}
+
+function prevDog(){
+  let userId = $("div.dog-show").data("uid");
+  let id = $("div.dog-show").data("id");
+  id = checkPrev(userId, id);
+  $.get("/users/" + userId + "/dogs/" + id, function(json){
+    const source = $("#dog-template").html();
+    const template = Handlebars.compile(source);
+    const content = template(json);
+    $("div.dog-show").hide().html(content).fadeIn();
+  });
+}
+
+function checkNext(userId, id){
+  let index = dogArray.indexOf(id);
+  if(index + 1 < dogArray.length){
+    $("div.dog-show").data("id", dogArray[index + 1]);
+    return dogArray[index + 1];
+  } else {
+    return dogArray[index];
+  }
+}
+
+function checkPrev(userId, id){
+  let index = dogArray.indexOf(id);
+  if(index - 1 >= 0){
+    $("div.dog-show").data("id", dogArray[index - 1]);
+    return dogArray[index - 1];
+  } else {
+    return dogArray[index];
+  }
+}
+
+function fillDogArray(){
+  let userId = $("div.dog-show").data("uid");
+  $.get("/users/" + userId + "/dogs", function(json){
+    json.forEach(obj => dogArray.push(obj.id));
+  });
+}
+
 $(function(){
   registerHelpers();
   listDogs();
   showDog();
+  fillDogArray();
+  $("div.dog-show").on('click', '#nextDog', function(){
+      nextDog();
+  });
+  $("div.dog-show").on('click', '#prevDog', function(){
+      prevDog();
+  });
 });

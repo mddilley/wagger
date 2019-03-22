fe08b0e64bfa9a842d36aa60be090c01418253af
Michael Dilley <mike.dilley@gmail.com>
2019-03-22 12:12:42 -0500

    Rename play_dates.js to play_date.js, create play_dates.js, add eventListener to 'create new playdate' button and preventDefault(), change js include tag in /play_dates/show and add include tag to /play_dates/index

--- app/assets/javascripts/play_dates.js
+++ app/assets/javascripts/play_dates.js
@@ -1,39 +1,7 @@
-class PlayDate{
-  constructor(obj){
-    this.date = obj.date
-    this.dogLimit = obj.dogLimit
-    this.dogs = obj.dogs
-    this.location = obj.location
-    this.id = obj.id
-    this.name = obj.name
-    this.userId = obj.userId
-    this.time = obj.time
-  }
-  formatTime(){
-    let time = moment(this.time);
-    time._isUTC = true;
-    this.time = time.format('LT');
-  }
-  formatDate(){
-    let date = moment(this.date)
-    date._isUTC = true;
-    this.date = date.format('LL');
-  }
-}
-
-function showPlaydate(){
-  let playDateId = $("div.playdate-show").data("id");
-  $.get("/play_dates/" + playDateId, function(json){
-    let playDate = new PlayDate(json);
-    playDate.formatTime();
-    playDate.formatDate();
-    const source = $("#playdate-template").html();
-    const template = Handlebars.compile(source);
-    const content = template(playDate);
-    $("div.playdate-show").hide().html(content).fadeIn();
-  });
-}
-
 $(function(){
-  showPlaydate();
+  $("a#playdate-new").click(function(e){
+    alert("boo")
+    e.preventDefault();
+  });
+
 });

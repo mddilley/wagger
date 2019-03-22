cd1ac4a0af3b31ca8b53199675c2326a73f8397c
Michael Dilley <mike.dilley@gmail.com>
2019-03-22 12:38:34 -0500

    Add event listener to 'create playdate' button in new playdate Handlebars form in showForm() in play_dates.js

--- app/assets/javascripts/play_dates.js
+++ app/assets/javascripts/play_dates.js
@@ -3,6 +3,10 @@ function showForm(){
   const template = Handlebars.compile(source);
   const content = template();
   $("div#playdate-new").hide().html(content).fadeIn();
+  $("div#playdate-new").on('click', '#playdate-create-button', function(e){
+    alert("hi")
+    e.preventDefault();
+  });
 }
 
 $(function(){

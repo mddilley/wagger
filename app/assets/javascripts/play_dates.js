2c1b09b17010cc6ac70e5321bedb8e9cdae3d0b5
Michael Dilley <mike.dilley@gmail.com>
2019-03-21 23:06:49 -0500

    Add Handlebars compile to showPlaydate() and append to /play_dates/show view in play_dates.js, remove erb of play_date attrs from /play_dates/show view

--- app/assets/javascripts/play_dates.js
+++ app/assets/javascripts/play_dates.js
@@ -1,7 +1,24 @@
+class PlayDate{
+  constructor(obj){
+    this.date = obj.date
+    this.dogLimit = obj.dogLimit
+    this.dogs = obj.dogs
+    this.location = obj.location
+    this.id = obj.id
+    this.name = obj.name
+    this.userId = obj.userId
+    this.time = obj.time
+  }
+}
+
 function showPlaydate(){
   let playDateId = $("div.playdate-show").data("id");
   $.get("/play_dates/" + playDateId, function(json){
-    debugger;
+    let playDate = new PlayDate(json);
+    const source = $("#playdate-template").html();
+    const template = Handlebars.compile(source);
+    const content = template(playDate);
+    $("div.playdate-show").hide().html(content).fadeIn();
   });
 }
 

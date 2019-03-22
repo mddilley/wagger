7023b9691f944a21142e33e3d92f7a44f83f920c
Michael Dilley <mike.dilley@gmail.com>
2019-03-22 12:29:36 -0500

    Add Handlebars create playdate template and change 'create playdate' id to 'playdate-new-button' in /play_dates/index, compile template and insert html into 'div#playdate-new' in play_dates.js

--- app/assets/javascripts/play_dates.js
+++ app/assets/javascripts/play_dates.js
@@ -1,6 +1,13 @@
+function showForm(){
+  const source = $("#playdate-new-template").html();
+  const template = Handlebars.compile(source);
+  const content = template();
+  $("div#playdate-new").hide().html(content).fadeIn();
+}
+
 $(function(){
-  $("a#playdate-new").click(function(e){
-    alert("boo")
+  $("a#playdate-new-button").click(function(e){
+    showForm();
     e.preventDefault();
   });
 

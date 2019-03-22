aa8ccdc361d695850f1ad3e2bf1db492a607518a
Michael Dilley <mike.dilley@gmail.com>
2019-03-22 10:09:29 -0500

    Add gem moment and sprocket to application.js, create formatTime() and formatDate() PlayDate prototype methods, add calls to showPlayDate() is play_dates.js

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'dotenv-rails'
 gem 'thin'
+gem 'momentjs-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/assets/javascripts/play_dates.js
+++ app/assets/javascripts/play_dates.js
@@ -9,12 +9,24 @@ class PlayDate{
     this.userId = obj.userId
     this.time = obj.time
   }
+  formatTime(){
+    let time = moment(this.time);
+    time._isUTC = true;
+    this.time = time.format('LT');
+  }
+  formatDate(){
+    let date = moment(this.date)
+    date._isUTC = true;
+    this.date = date.format('LL');
+  }
 }
 
 function showPlaydate(){
   let playDateId = $("div.playdate-show").data("id");
   $.get("/play_dates/" + playDateId, function(json){
     let playDate = new PlayDate(json);
+    playDate.formatTime();
+    playDate.formatDate();
     const source = $("#playdate-template").html();
     const template = Handlebars.compile(source);
     const content = template(playDate);
